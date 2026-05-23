37dda35628cc8720816beca3232c410ba6dda27d
zixian Jiang <115254510+jzx531@users.noreply.github.com>
2026-05-23 20:03:53 +0800

    Update AIStrategy.h

--- AIApps/ChatServer/include/AIUtil/AIStrategy.h
+++ AIApps/ChatServer/include/AIUtil/AIStrategy.h
@@ -41,9 +41,76 @@ public:
     std::string getApiKey() const override;
     std::string getModel() const override;
 
-    
+    json buildRequest(const std::vector<std::pair<std::string,long long>> & messages) const override;
+    std::string parseResponse(const json & response) const override;
 
-}
+private:
+    std::string apiKey_;
+};
+
+class DouBaoStrategy : public AIStrategy {
+
+public:
+    DouBaoStrategy() {
+        const char* key = std::getenv("DOUBAO_API_KEY");
+        if (!key) throw std::runtime_error("DOUBAO API Key not found!");
+        apiKey_ = key;
+        isMCPModel = false;
+    }
+    std::string getApiUrl() const override;
+    std::string getApiKey() const override;
+    std::string getModel() const override;
+
+    json buildRequest(const std::vector<std::pair<std::string, long long>>& messages) const override;
+    std::string parseResponse(const json& response) const override;
+
+private:
+    std::string apiKey_;
+};
+
+class AliyunRAGStrategy : public AIStrategy {
+
+public:
+    AliyunRAGStrategy() {
+        const char* key = std::getenv("DASHSCOPE_API_KEY");
+        if (!key) throw std::runtime_error("Aliyun API Key not found!");
+        apiKey_ = key;
+        isMCPModel = false;
+    }
+
+    std::string getApiUrl() const override;
+    std::string getApiKey() const override;
+    std::string getModel() const override;
+
+    json buildRequest(const std::vector<std::pair<std::string, long long>>& messages) const override;
+    std::string parseResponse(const json& response) const override;
+
+private:
+    std::string apiKey_;
+};
+
+class AliyunMcpStrategy : public AIStrategy {
+
+public:
+    AliyunMcpStrategy() {
+        const char* key = std::getenv("DASHSCOPE_API_KEY");
+        if (!key) throw std::runtime_error("Aliyun API Key not found!");
+        apiKey_ = key;
+        isMCPModel = true;
+    }
+
+    std::string getApiUrl() const override;
+    std::string getApiKey() const override;
+    std::string getModel() const override;
+
+    json buildRequest(const std::vector<std::pair<std::string, long long>>& messages) const override;
+    std::string parseResponse(const json& response) const override;
+
+private:
+    std::string apiKey_;
+};
+
+
 
 #endif
 
